61742062c071bfcedaded87dd7e4dd770d333e84
kittleik <sirkittelsen@gmail.com>
2014-03-21 20:37:54 +0100

    hei

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,7 @@
 angular.module('powerApp')
   .controller('MainCtrl', function ($rootScope, $scope, $http, $firebase, Auth, simpleLogin) {
 	$scope.method = 'GET';
-	$scope.url = 'https://api.demosteinkjer.no/downloads/361';
+	$scope.url = 'https://api.demosteinkjer.no/meters/0043205c065d421dbc187464abfd2895/latest?seriesType=ActivePlus';
 
     Auth.setCredentials("3749f5da4f0d427faf9ed00bb616576e", "7bf19829a91144028101feb1740bafb9");
 
@@ -19,6 +19,7 @@ angular.module('powerApp')
 			.success(function(data, status) {
 				console.log(data);
 				console.log(status)
+				console.log(data.
 			})
 			.error(function(data, status) {
 				console.log(data || "Request failed");
