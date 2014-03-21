094861177070be6d99be6ca83149b3c908385e27
hua <anders.hua@gmail.com>
2014-03-21 18:04:52 +0100

    Login + mye filer, services osv. Kommentert ut navbar og annet dritt.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,13 +5,21 @@ angular.module('powerApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
+  'angularfire.firebase',
+  'angularfire.login',
   'firebase'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
+        authRequired: true,
         templateUrl: 'partials/main',
         controller: 'MainCtrl'
+      })
+      .when('/login', {
+        authRequired: false,
+        templateUrl: 'partials/login',
+        controller: 'LoginCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('powerApp')
-  .controller('MainCtrl', function ($scope, $http, $firebase) {
+  .controller('MainCtrl', function ($rootScope, $scope, $http, $firebase, simpleLogin) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
 
-    $scope.items = $firebase(new Firebase('https://powerhack.firebaseio.com/'));
+  $scope.items = $firebase(new Firebase('https://powerhack.firebaseio.com/'));
 
 	$scope.testApi = $http({method: 'GET', url: 'https://api.demosteinkjer.no'}).
 		success(function(data, status, headers, config) {
@@ -15,4 +15,15 @@ angular.module('powerApp')
 		error(function(data, status, headers, config) {
 		  console.log(data);
 		});
+
+	$scope.logout = function() {
+      simpleLogin.logout(function(err) {
+        $scope.err = err? err + '' : null;
+      });
+	  };
+
+	$scope.testApi = function(){
+		console.log($rootScope.auth);
+	  };
+
   });
