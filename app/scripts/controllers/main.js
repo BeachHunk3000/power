85dc7f1cfc4f6a5585e91312d14b7575e4818a4b
Jørgen Svennevik Notland <jorgen.notland@gmail.com>
2014-03-21 23:39:42 +0100

    småfiks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,33 +2,7 @@
 
 angular.module('powerApp')
   .controller('MainCtrl', function ($rootScope, $scope, $http, $firebase, Auth, simpleLogin, firebaseRef) {
-	$scope.method = 'GET';
-	$scope.url = 'https://api.demosteinkjer.no/meters/0e6e348bfdb74432b6709526527c3d12/latest?seriesType=ActivePlus';
-
     Auth.setCredentials("3749f5da4f0d427faf9ed00bb616576e", "7bf19829a91144028101feb1740bafb9");
 
-	$scope.fetch = function() {
-		//$scope.code = null;
-		//$scope.response = null;
-
-		//$http({method: 'GET', url: 'www.google.com/someapi', headers: {'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='}});
-		$http({method: $scope.method,
-			url: $scope.url,
-			headers: {'Content-type': 'application/json'}})
-			.success(function(data, status) {
-				console.log(data);
-				console.log(status)
-			})
-			.error(function(data, status) {
-				console.log(data || "Request failed");
-				console.log(status);
-			});
-	};
-
-  $scope.logout = function() {
-      simpleLogin.logout(function(err) {
-        $scope.err = err? err + '' : null;
-      });
-    };
 
   });
