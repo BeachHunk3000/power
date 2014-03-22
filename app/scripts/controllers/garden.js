d475886a43502cc0d9c0c84af037ae7642250b62
Jørgen Svennevik Notland <jorgen.notland@gmail.com>
2014-03-22 08:45:50 +0100

    garden funksjonalitet

--- app/scripts/controllers/garden.js
+++ app/scripts/controllers/garden.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('powerApp')
-  .controller('GardenCtrl', function ($rootScope, $scope, $http, $firebase, Auth, simpleLogin, firebaseRef) {
+  .controller('GardenCtrl', function ($rootScope, $scope, $http, $firebase, Auth, simpleLogin, firebaseRef, syncData) {
   
- 
+	$scope.items = syncData('/users/' + $rootScope.auth.user.uid + '/purchased_items/');
+	$scope.store_open = false;
 
   });
